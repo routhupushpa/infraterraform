625a2f68a47e3019e4392983846f4b2f4de08ed9
routhupushpa <129184894+routhupushpa@users.noreply.github.com>
2023-07-11 13:38:37 +0530

    Update backend.tf
    change ap-south-1 to us-east-1 region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "hashtek-terraform-state-bucket"
     key = "main"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "hashtekTerraformStatelock"
   }
 }
